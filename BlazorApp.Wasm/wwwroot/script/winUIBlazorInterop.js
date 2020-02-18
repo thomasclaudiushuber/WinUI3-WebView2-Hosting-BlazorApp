00780444ad1e2de86e405fa8572670dbb49f1d52
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2020-02-18 09:55:03 +0100

    Add project files.

---
+++ BlazorApp.Wasm/wwwroot/script/winUIBlazorInterop.js
@@ -0,0 +1,11 @@
+﻿var interopFunctions = {};
+
+interopFunctions.registerIndexComponent = (dotnetObj) => {
+  interopFunctions.indexComponent = dotnetObj;
+};
+
+interopFunctions.setFirstName = (firstName) => {
+  console.log("JavaScript setFirstName executed");
+
+  interopFunctions.indexComponent.invokeMethodAsync('SetFirstName', firstName);
+};
---
+++ WinUIHostingChromiumWebView/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace WinUIHostingChromiumWebView
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+      webView2.UriSource = new Uri("https://localhost:44305/");
+    }
+
+    private async void ButtonSetFirstName_Click(object sender, RoutedEventArgs e)
+    {
+      await webView2.ExecuteScriptAsync($"interopFunctions.setFirstName('{txtFirstName.Text}');");
+    }
+  }
+}
